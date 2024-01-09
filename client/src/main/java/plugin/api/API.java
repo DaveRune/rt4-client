70c3455df8527475c6b855ee6f49ba7725d90a07
ipkpjersi <ipkpjersi@gmail.com>
2024-01-08 19:10:51 -0500

    Fixed screenshot NPE when not logged in

--- client/src/main/java/plugin/api/API.java
+++ client/src/main/java/plugin/api/API.java
@@ -276,7 +276,8 @@ public class API {
     public static void Screenshot(String... subfolders) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd-HH_mm_ss");
         String dateTime = dateFormat.format(new Date());
-        client.instance.saveScreenshot(PlayerList.self.username.toString() + "_" + dateTime + ".png", subfolders);
+        String username = PlayerList.self != null && PlayerList.self.username != null && !PlayerList.self.username.toString().isEmpty() ? PlayerList.self.username.toString() : "2009Scape";
+        client.instance.saveScreenshot( username + "_" + dateTime + ".png", subfolders);
     }
 
     public static void PlaySound(int volume, int trackId, int delay) {
